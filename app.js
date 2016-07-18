76e00a03b2f248848e7cce6029df4f2a4941330b
ronan <ronan@T430>
2016-07-18 04:49:55 +0200

    Adding templating engine and start improving organisation

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app = express();
 
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'pug');
+
 app.get('/', function (req, res) {
-      res.send('Hello World!');
+    res.render('index', { title: 'Hippymood', message: 'Hello there!'});
 });
 
-app.listen(8080, function () {
-      console.log('Example app listening on port 8080!');
+app.use('/music', express.static('music'));
+
+app.listen(app.get('port'), function () {
+      console.log('Example app listening on port ' + app.get('port') + '!');
 });
--- id3tags_test.js
+++
@@ -1,5 +0,0 @@
-var id3 = require('id3js');
-
-id3({ file: './music/06 Donde Estas, Yolanda_.mp3', type: id3.OPEN_LOCAL }, function(err, tags) {
-    console.log("Information sur la piste : " + JSON.stringify(tags));
-});
