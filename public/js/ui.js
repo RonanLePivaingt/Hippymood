e10604f3f7994bb7adfc4b775eb2ac7809f28b79
ronan <ronan@T430>
2016-07-23 02:44:03 +0200

    UI : the playing genre is colored + play/pause button bugfix

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,3 +1,16 @@
+/* 
+ * Useful generic functions 
+ */
+function addClass(el, className) {
+    if (el.classList) el.classList.add(className);
+    else if (!hasClass(el, className)) el.className += ' ' + className;
+}
+
+function removeClass(el, className) {
+    if (el.classList) el.classList.remove(className);
+    else el.className = el.className.replace(new RegExp('\\b'+ className+'\\b', 'g'), '');
+}
+
 function getAjax(url, success) {
     var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
     xhr.open('GET', url);
@@ -9,55 +22,9 @@ function getAjax(url, success) {
     return xhr;
 }
 
-var genres = document.getElementsByClassName("genreItem");
-
-var currentGenreHref = "";
-
-var playGenre = function() {
-    var dataHref = this.getAttribute("data-href");
-    currentGenreHref = dataHref;
-    getAjax(dataHref, function(data){ 
-        data = JSON.parse(data);
-        player(data.randomSong);
-    });
-};
-
-for (var i = 0; i < genres.length; i++) {
-    genres[i].addEventListener('click', playGenre, false);
-}
-
-function playNext(){
-    console.log("Chargement de la prochaine chanson.");
-    getAjax(currentGenreHref, function(data){ 
-        data = JSON.parse(data);
-        player(data.randomSong);
-    });
-}
-function playPause(){
-    var player = document.querySelector("#player");
-    var buttonPause = document.querySelector("#pause");
-    var buttonPlay = document.querySelector("#play");
-    if (player.paused) {
-        player.play();
-        buttonPlay.style.display = 'none';
-        buttonPause.style.display = 'inline-block';
-
-    }
-    else {
-        player.pause();
-        buttonPause.style.display = 'none';
-        buttonPlay.style.display = 'inline-block';
-    }
-}
-
-function player(songData) {
-    var player = document.getElementById("player");
-    player.setAttribute("src", songData.path);
-
-    playerVue.play(songData);
-}
-
-// Vue.js
+/*
+ * Vue.js
+ */
 var playerVue = new Vue({
     el: '#app',
     data: {
@@ -74,8 +41,86 @@ var playerVue = new Vue({
     }
 });
 
+/*
+ * Global vars
+ */
+var genres = document.getElementsByClassName("genreItem");
+
+var player = document.querySelector("#player");
+var buttonPause = document.querySelector("#pause");
+var buttonPlay = document.querySelector("#play");
+
+var lastGenre = '';
+var currentGenre;
+
+
+/*
+ * Player functions
+ */
+function play(songData) {
+    var player = document.getElementById("player");
+    player.setAttribute("src", songData.path);
+
+    playerVue.play(songData);
+}
+function playGenre() {
+    // Saving genre information
+    lastGenre = currentGenre;
+    currentGenre = this;
+
+    var dataHref = this.getAttribute("data-href");
+    getAjax(dataHref, function(data){ 
+        data = JSON.parse(data);
+        play(data.randomSong);
+        genreButtonStyle();
+        playPause();
+    });
+};
+function playNext(){
+    console.log("Chargement de la prochaine chanson.");
+    getAjax(currentGenreHref, function(data){ 
+        data = JSON.parse(data);
+        play(data.randomSong);
+        playPause();
+    });
+}
+
+/*
+ * Player functions for the UI
+ */
+function playPause(){
+    if (player.paused) {
+        player.play();
+        buttonPlay.style.display = 'none';
+        buttonPause.style.display = 'inline-block';
+
+    }
+    else {
+        player.pause();
+        buttonPause.style.display = 'none';
+        buttonPlay.style.display = 'inline-block';
+    }
+}
+
+/*
+ * Events
+ */
+// Playing genre on click
+for (var i = 0; i < genres.length; i++) {
+    genres[i].addEventListener('click', playGenre, false);
+}
+
 // Playing another song at the end of each songs
 document.querySelector("#player").addEventListener("ended", playNext, false);
+
 // Play pause song
 document.querySelector("#play").addEventListener("click", playPause, false);
 document.querySelector("#pause").addEventListener("click", playPause, false);
+
+function genreButtonStyle() {
+    // Check lastGenre as it is undefined on first execution
+    if (lastGenre)
+        removeClass(lastGenre,"mdl-button--colored");
+
+    addClass(currentGenre,"mdl-button--colored");
+}
