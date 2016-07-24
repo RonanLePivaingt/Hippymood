65e06d19742bb1f38461903159c05cb0c63451f2
ronan <ronan@T430>
2016-07-24 13:19:23 +0200

    Scan is synchronous now to support large song collections

--- scan.js
+++ scan.js
@@ -3,37 +3,28 @@ var id3tags = require('./id3tags');
 
 var fs = require('fs');
 var path = require('path');
-var walk = function(dir, done) {
-    var results = [];
-    fs.readdir(dir, function(err, list) {
-        if (err) return done(err);
-        var pending = list.length;
-        if (!pending) return done(null, results);
-        list.forEach(function(file) {
-            file = path.resolve(dir, file);
-            fs.stat(file, function(err, stat) {
-                if (stat && stat.isDirectory()) {
-                    walk(file, function(err, res) {
-                        results = results.concat(res);
-                        if (!--pending) done(null, results);
-                    });
-                } else {
-                    results.push(file);
-                    if (!--pending) done(null, results);
-                }
-            });
-        });
-    });
-};
 
-function listProcessing(IDontGetWhatThisVarIsFor, results) {
-    results.forEach(function(name, index) {
-    // Processing only mp3 files
-        if (name.slice(-3) == "mp3") {
-            id3tags.scan(name);
-        }
-    });
+var walk = function(dir) {
+    var results = []
+    var list = fs.readdirSync(dir)
+    list.forEach(function(file) {
+        file = dir + '/' + file
+        var stat = fs.statSync(file)
+        if (stat && stat.isDirectory()) results = results.concat(walk(file))
+        else results.push(file)
+    })
+    return results
 }
+var files = walk("music");
+// loop through array with all new ids
+var i = 0, l = files.length;
+(function iterator() {
+    var filename = files[i];
+    if (filename.slice(-3) == "mp3") {
+        id3tags.scan(filename);
+    }
 
-walk("music", listProcessing);
-//walk("music/The Heavy", listProcessing);
+    if(++i<l) {
+        setTimeout(iterator, 50);
+    }
+})();
