c396f49acc54ddb99544e55ea3d123632bbd125a
ronan <ronan@T430>
2016-09-23 14:15:34 +0200

    Test de bouge ta chips

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
+    // Keyboard unlock
 	var combination = '';
 
 	document.onkeyup = function(evt){    
@@ -46,15 +47,26 @@ document.addEventListener("DOMContentLoaded", function(event) {
                     display();
                 }
             });
-            /*
-			var audio = new Audio('Mushroom.mp3');
-			audio.play();
-			audio.addEventListener(
-				"ended", 
-				function() { window.location = "/library";}
-				, true
-			);
-            */
 		}
 	}
+
+    // Touch unlock
+    $( "#datChips" ).draggable({ revert: true });
+    var myElement = document.getElementById('auth');
+    //var myElement = document.querySelectorAll('window');
+
+    // create a simple instance
+    // by default, it only adds horizontal recognizers
+    var mc = new Hammer(myElement);
+
+    // let the pan gesture support all directions.
+    // this will block the vertical scrolling on a touch-device while on the element
+    mc.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+
+    // listen to events...
+    var debug = document.getElementById('debug');
+    mc.on("panleft panright panup pandown tap press", function(ev) {
+        console.log(ev.type +" gesture detected.")
+        debug.innerHTML = debug.innerHTML +";" + ev.type +" gesture detected.";
+    });
 });
