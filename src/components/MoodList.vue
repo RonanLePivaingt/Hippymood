448b0f342421a9c5133b31ba278e2da56a0e2dda
Ronan Le Pivaingt <r.lepivaingt@gmail.com>
2017-03-15 13:33:31 +0100

    Webpack integration : checkpoint 1

---
+++ src/components/MoodList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="mood-list">
+    <h2> Hello mood list </h2>
+    <button 
+      @click="play" 
+      :id="mood.id" 
+      class="mdc-button" 
+      :class="isActive"
+    >
+      {{ mood.name }}
+    </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'mood-list',
+    props: ['mood', 'currentmood'],
+    methods: {
+      play: function (el) {
+        // vm.playGenre(el.target.id);
+      }
+    },
+    computed: {
+      isActive: function () {
+        // Return the CSS classes to apply to the current mood button
+        return this.currentmood === this.mood.id ? 'mdc-button--accent mdc-button--raised' : ''
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
