0dc1546544e5f3ed3b8f54e79681bb0bb5ff37e1
Ronan Le Pivaingt <r.lepivaingt@gmail.com>
2017-03-16 07:56:52 +0100

    Intégration webpack : checkpoint 2

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,33 @@
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data: function () {
+    return {
+      message: 'Yop',
+      link: 'http://jaimeleschips.fr',
+      success: true,
+      moods: [],
+      current: {},
+      infos: {}
+    }
+  },
+  methods: {
+    playGenre: function (genreId) {
+      console.log('Play')
+    },
+    resetSession: function () {
+      console.log('Reset session')
+    }
+  },
+  computed: {
+    currentmood: function () {
+      return this.current.genreId
+    }
+  },
+  created: function () {
+    console.log('Get the mood list')
+  }
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Player from '@/components/Player'
 import MoodList from '@/components/MoodList'
 
 Vue.use(Router)
@@ -9,11 +10,16 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Player',
+      component: Player
+    },
+    {
+      path: '/Hello',
       name: 'Hello',
       component: Hello
     },
     {
-      path: '/yo',
+      path: '/MoodList',
       name: 'MoodList',
       component: MoodList
     }
