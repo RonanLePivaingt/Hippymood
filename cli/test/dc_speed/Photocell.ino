e4af3882b9de381b48ed28fe1d9ee2ad1204e6df
Ronan Le Pivaingt <r.lepivaingt@gmail.com>
2019-07-07 10:38:06 +0000

    Working Photosensor with rotation count

--- cli/test/dc_speed/Photocell.ino
+++ cli/test/dc_speed/Photocell.ino
@@ -1,19 +1,23 @@
 int LDR = A0;            // LDR input at A0 pin.  
 int LDRReading = 0;      // to store input value of LDR  
 int RotationCount = 0;   // to count rotations  
-int threshold_val = 300; // set threshold value of LDR. This will vary with your LED and LDR.
+int threshold_val = 450; // set threshold value of LDR. This will vary with your LED and LDR.
+int dark = 1;
 
 void setup() {  
   Serial.begin(9600);     // initializing serial communication.
 }
 void loop() {  
   LDRReading = analogRead(LDR);    // Reading LDR
-  if (LDRReading > threshold_val)  // Varies with white and black. Set at value between the two
+  if (LDRReading > threshold_val && dark == 0)  // Varies with white and black. Set at value between the two
   {
     RotationCount ++; // Add one to rotation count
+    dark = 1;
+  } else if (LDRReading < threshold_val) {
+      dark = 0;
   }
   Serial.println(LDRReading);    // Print LDR Reading
-  // Serial.println(RotationCount); // Printing LDR count
+  Serial.println(RotationCount); // Printing LDR count
 
   delay(50);                      // use for testing LDR reading
 }
