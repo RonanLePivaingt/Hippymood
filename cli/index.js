cdea9d83ebae298ed36cc41fcdfdee1a1d832a76
Ronan Le Pivaingt <r.lepivaingt@gmail.com>
2019-08-01 16:26:52 +0000

    Avoid starting again mood when tag is detected multiple times

--- cli/index.js
+++ cli/index.js
@@ -1,19 +1,25 @@
 // Conf and RFID dependencies
-var rc522 = require("rc522");
-var db = require('./db.js');
-var mpc = require('./mpc.js');
+let rc522 = require("rc522");
+let db = require('./db.js');
+let mpc = require('./mpc.js');
 
 // Loading associative mood list
 db.getMoodList.then(data => {
-    associativeMoodList = data;
+    let associativeMoodList = data;
+    let lastMoodRfidSerialNumber = '';
 
     console.log('Ready !');
 
     // On vinyle detection
     rc522(function(rfidSerialNumber){
-        db.getSerialNumberMoodName(rfidSerialNumber, associativeMoodList)
-            .then(moodName => db.getMoodSongs(moodName))
-            .then(songs => mpc.playSongs(songs))
-        ;
+        // Checking if the mood is already played
+        if (rfidSerialNumber !== lastMoodRfidSerialNumber) {
+            lastMoodRfidSerialNumber = rfidSerialNumber;
+
+            db.getSerialNumberMoodName(rfidSerialNumber, associativeMoodList)
+                .then(moodName => db.getMoodSongs(moodName))
+                .then(songs => mpc.playSongs(songs))
+            ;
+        }
     });
 });
