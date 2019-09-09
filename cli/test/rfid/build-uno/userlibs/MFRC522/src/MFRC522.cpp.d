e47c20feb23ba3198d78bfd0b5e47f24e4d449a7
Ronan Le Pivaingt <r.lepivaingt@gmail.com>
2019-09-09 10:08:10 +0000

    [TASK] RFID reading on Arduino instead of RPi : test case

---
+++ cli/test/rfid/build-uno/userlibs/MFRC522/src/MFRC522.cpp.d
@@ -0,0 +1,16 @@
+build-uno/userlibs/MFRC522/src/MFRC522.cpp.o: \
+ /home/pi/dev/Hippymood/cli/test/rfid/lib/MFRC522/src/MFRC522.cpp \
+ /usr/share/arduino/hardware/arduino//cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/WCharacter.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/new.h \
+ /usr/share/arduino/hardware/arduino//variants/standard/pins_arduino.h \
+ /home/pi/dev/Hippymood/cli/test/rfid/lib/MFRC522/src/MFRC522.h \
+ /home/pi/dev/Hippymood/cli/test/rfid/lib/MFRC522/src/require_cpp11.h \
+ /home/pi/dev/Hippymood/cli/test/rfid/lib/MFRC522/src/deprecated.h \
+ /home/pi/dev/Hippymood/cli/test/rfid/lib/SPI/SPI.h
