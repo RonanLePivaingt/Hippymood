a01ddf5b7da28e176580bda096b8d3d1b9d718c9
Ronan Le Pivaingt <r.lepivaingt@gmail.com>
2019-11-06 10:55:13 +0100

    NODE_ENV set fot frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false
+if (process.env.NODE_ENV === 'development') {
+  // Display user timings in dev tools to check components performance in browser dev tools
+  // See more on Vue Dose : https://vuedose.tips/tips/measure-runtime-performance-in-vue-js-apps
+  Vue.config.performance = true
+
+  Vue.config.productionTip = false
+} else if (process.env.NODE_ENV === 'production') {
+  Vue.config.silent = true
+}
 
 new Vue({
   vuetify,
