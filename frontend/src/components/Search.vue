a790ba1beb7bae8c388adebace2a83fd9cf43301
Ronan Le Pivaingt <r.lepivaingt@gmail.com>
2020-04-21 12:18:27 +0200

    Search test

--- frontend/src/components/Search.vue
+++ frontend/src/components/Search.vue
@@ -12,50 +12,73 @@
     />
 
     <v-list>
-      <v-list-item
-        v-for="song in searchResults"
-        :key="song.id"
-        class="pa-0"
-        :two-line="!song.album ? true : false"
-        :three-line="song.album ? true : false"
-        :disabled="videoMode && !song.youtube ? true : false"
-        @click="play(song)"
-      >
-        <v-list-item-content class="pa-0">
-          <v-row
-            class="pr-1 ma-0"
-            justify="space-between"
-            align="center"
-          >
-            <v-col class="metadata flex-wrap pl-0">
-              <v-list-item-title>
-                <v-icon>mdi-music-note</v-icon>
-                {{ song.song }}
-              </v-list-item-title>
-              <v-list-item-subtitle v-show="song.album">
-                <v-icon>mdi-album</v-icon>
-                {{ song.album }}
-              </v-list-item-subtitle>
-              <v-list-item-subtitle>
-                <v-icon>mdi-account</v-icon>
-                {{ song.artist }}
-              </v-list-item-subtitle>
-            </v-col>
-
-            <v-btn
-              :disabled="videoMode && !song.youtube ? true : false"
+      <transition-group name="fade">
+        <v-list-item
+          v-for="song in searchResults"
+          :key="song.id"
+          class="pa-0"
+          :two-line="!song.album ? true : false"
+          :three-line="song.album ? true : false"
+          :disabled="videoMode && !song.youtube ? true : false"
+          @click="!song.isLoader ? play(song) : ''"
+        >
+          <v-list-item-content class="pa-0">
+            <v-row
+              class="pr-1 ma-0"
+              justify="space-between"
+              align="center"
             >
-              <v-icon
-                v-show="videoMode && song.youtube"
-                left
+              <v-col class="metadata flex-wrap pl-0">
+                <v-list-item-title>
+                  <v-icon>mdi-music-note</v-icon>
+                  {{ song.song }}
+                  <v-skeleton-loader
+                    v-show="song.songWidth"
+                    type="text"
+                    :style="song.songWidth"
+                  />
+                </v-list-item-title>
+                <v-list-item-subtitle v-show="song.album">
+                  <v-icon>mdi-album</v-icon>
+                  {{ song.album }}
+                  <v-skeleton-loader
+                    v-show="song.albumWidth"
+                    type="text"
+                    :style="song.albumWidth"
+                  />
+                </v-list-item-subtitle>
+                <v-list-item-subtitle>
+                  <v-icon>mdi-account</v-icon>
+                  {{ song.artist }}
+                  <v-skeleton-loader
+                    v-show="song.artistWidth"
+                    type="text"
+                    :style="song.artistWidth"
+                  />
+                </v-list-item-subtitle>
+              </v-col>
+
+              <v-skeleton-loader
+                class="mood-skeleton"
+                type="button"
+                :style="song.btnWidth"
               >
-                mdi-youtube
-              </v-icon>
-              {{ song.mood }}
-            </v-btn>
-          </v-row>
-        </v-list-item-content>
-      </v-list-item>
+                <v-btn
+                  :disabled="videoMode && !song.youtube ? true : false"
+                >
+                  <v-icon
+                    v-show="videoMode && song.youtube"
+                    left
+                  >
+                    mdi-youtube
+                  </v-icon>
+                  {{ song.mood }}
+                </v-btn>
+              </v-skeleton-loader>
+            </v-row>
+          </v-list-item-content>
+        </v-list-item>
+      </transition-group>
     </v-list>
 
     <v-col
@@ -77,6 +100,19 @@
 import _ from 'lodash'
 import music from '../api/music'
 import { mapState, mapActions } from 'vuex'
+
+// Generating array of skeleton items with various width
+const nbLoaders = 3,
+      min = 75,
+      max = 200,
+      loaders = Array.from(Array(nbLoaders).keys()).map((val, index) => ({
+        id: index,
+        songWidth: `width: ${Math.max(372 * Math.random(), 122)}px;`,
+        albumWidth: `width: ${Math.max(372 * Math.random(), 122)}px;`,
+        artistWidth: `width: ${Math.max(372 * Math.random(), 122)}px;`,
+        btnWidth: `width: ${Math.max(max * Math.random(), min)}px;`,
+        isLoader: true,
+      }));
 
 export default {
   name: 'Search',
@@ -104,6 +140,8 @@ export default {
       if (val) {
         this.loading = true
         this.searchValue = val
+        this.searchResults = loaders
+        this.noResult = false
 
         music.search(val).then(res => {
           this.loading = false
@@ -154,5 +192,18 @@ export default {
   .v-btn > .v-btn__content .v-icon.theme--light {
     color: rgba(0, 0, 0, 0.54);
   }
+}
+.mood-skeleton {
+  display: inline-flex;
+
+  > button {
+    flex-grow: 1;
+  }
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
